aa0024f1d4c8d8b0352cf054c67a82c551e77ab6
strhzy <143874903+strhzy@users.noreply.github.com>
2024-01-26 20:13:56 +0300

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,21 +8,21 @@ import java.util.Formatter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int age;
+        int age;//обьявляем переменные
         float marks;
         String name;
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);//обьявляем сканер для ввода данных
         System.out.print("Введите имя студента: ");
-        name = in.nextLine();
+        name = in.nextLine();//ввод имени
         System.out.print("Введите возраст студента: ");
-        age = in.nextInt();
+        age = in.nextInt();//ввод возраста
         System.out.print("Введите средний балл студента: ");
-        marks = in.nextFloat();
-        FileWriter writer = new FileWriter("students.txt", true);
+        marks = in.nextFloat();//ввод среднего балла
+        FileWriter writer = new FileWriter("students.txt", true);//обьявление файлврайтера для добавления текста
         BufferedWriter bw = new BufferedWriter(writer);
-        String student = String.format("Имя: %s, возраст: %s, средний балл: %s",name, Integer.toString(age), Float.toString(marks));
-        bw.write(student);
+        String student = String.format("Имя: %s, возраст: %s, средний балл: %s",name, Integer.toString(age), Float.toString(marks));//форматирование строки для красоты
+        bw.write(student);//добавление данных
         bw.newLine();
-        bw.close();
+        bw.close();//закрытие файл врайтера
     }
 }
